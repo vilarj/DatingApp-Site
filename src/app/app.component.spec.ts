d640b3b5878ed29b846d00bedc0d88816cf99be0
Vilarj <lamaravillasan@outlook.com>
2024-06-10 17:04:33 -0400

    Downgrading the project to angular 16

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,11 +2,9 @@ import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [AppComponent],
-    }).compileComponents();
-  });
+  beforeEach(() => TestBed.configureTestingModule({
+    declarations: [AppComponent]
+  }));
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -14,16 +12,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'DatingApp-Site' title`, () => {
+  it(`should have as title 'DatingApp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('DatingApp-Site');
+    expect(app.title).toEqual('DatingApp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, DatingApp-Site');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('DatingApp app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import {HttpClient, HttpClientModule} from '@angular/common/http';
-// import {provideHttpClient} from '@angular/common/http';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, HttpClientModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  title: string = 'DatingApp-Site';
-  users: any;
-  
-  constructor(private http: HttpClient) {
-
-  }
-  ngOnInit(): void {
-    this.http.get('http://localhost:5000/api/user').subscribe({
-      next: response => {this.users = response; console.log(response)},
-      error: error => {console.log('Error:\n ' + JSON.stringify(error))},
-      complete: () => {console.log('Request completed')}
-    });
-  }
-
+export class AppComponent {
+  title = 'DatingApp';
 }
