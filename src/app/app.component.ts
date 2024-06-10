b907f908c04acb5093cb06e045e0d436821d3c20
Vilarj <lamaravillasan@outlook.com>
2024-06-10 17:27:38 -0400

    Sucessfully making the API call for our backend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'DatingApp';
+export class AppComponent implements OnInit {
+  title = 'DatingApp-Site';
+  users: any;
+
+
+  constructor(private http: HttpClient) {
+
+  }
+
+
+  ngOnInit(): void {
+    this.http.get('http://localhost:5000/api/user').subscribe({
+      next: response => {this.users = response},
+      error: error => {console.log('Error:\n' + JSON.stringify(error))},
+      complete: () => {console.log('Request completed')}
+    });
+  }
+
 }
