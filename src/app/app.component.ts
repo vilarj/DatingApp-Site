c18d555ba704fa719d0ca80992e985c8afd15616
Vilarj <lamaravillasan@outlook.com>
2024-06-10 15:45:04 -0400

    Making Http calls to our backned sucessfully

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+// import {provideHttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'DatingApp-Site';
+export class AppComponent implements OnInit {
+  title: string = 'DatingApp-Site';
+  users: any;
+  
+  constructor(private http: HttpClient) {
+
+  }
+  ngOnInit(): void {
+    this.http.get('http://localhost:5000/api/user').subscribe({
+      next: response => {this.users = response; console.log(response)},
+      error: error => {console.log('Error:\n ' + JSON.stringify(error))},
+      complete: () => {console.log('Request completed')}
+    });
+  }
+
 }
